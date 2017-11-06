96d1fffc03d6be1c9ad6c1fc738cb034f82099c3
rabin <rabin@dever.cc>
2017-11-06 06:51:33 +0000

    edit

--- src/docker/build/iot/nongxiaohe/demeter.sh
+++ src/docker/build/iot/nongxiaohe/demeter.sh
@@ -10,23 +10,23 @@ start_demeter()
     chmod -R +x $DEMETER_HOME/*.py
     install.py
     process_start admin.py
+    process_start web.py
     process_start front.py
     process_start sub.py
-    process_start cron.py
 }
 
 stop_demeter()
 {
 	process_stop admin.py
+    process_stop web.py
     process_stop front.py
     process_stop sub.py
-    process_stop cron.py
 }
 
 monit_demeter()
 {
     process_monit admin.py
+    process_monit web.py
     process_monit front.py
     process_monit sub.py
-    process_monit cron.py
 }
