8ca16782a85829c86abe2d7d567b9adaaed4f931
rabin <rabin@dever.cc>
2017-11-19 01:23:18 +0000

    edit

--- container/conf/java/hadoop/yarn-env.sh
+++ container/conf/java/hadoop/yarn-env.sh
@@ -32,7 +32,7 @@ if [ "$JAVA_HOME" = "" ]; then
 fi
 
 JAVA=$JAVA_HOME/bin/java
-JAVA_HEAP_MAX=-Xmx2048m 
+JAVA_HEAP_MAX=-Xmx3072m 
 
 # For setting YARN specific HEAP sizes please use this
 # Parameter and set appropriately
