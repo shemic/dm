d63776bab8010200b7a4d738b6788d7d969bc2a7
rabin <rabin@dever.cc>
2017-11-30 04:28:59 +0000

    edit

--- src/docker/build/elastic/elasticsearch/elasticsearch.sh
+++ src/docker/build/elastic/elasticsearch/elasticsearch.sh
@@ -3,9 +3,9 @@ set -e
 
 start_elasticsearch()
 {
-	chmod -R 777 /share/process/elasticsearch
-	chmod -R 777 $ES_HOME/config/scripts/
-   	process_start elasticsearch -d
+	#chmod -R 777 /share/process/elasticsearch
+	#chmod -R 777 $ES_HOME/config/scripts/
+	process_start elasticsearch -d
 }
 
 stop_elasticsearch()
