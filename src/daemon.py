3b2a67c4ab242d456d183f6c5f09790fb1dc5828
dever <dm@dever.cc>
2019-01-18 07:44:41 +0000

    edit

--- src/daemon.py
+++ src/daemon.py
@@ -1,54 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 用于定时运行容器
+# dm_process为key，value为具体指令 如：dm call office-convert_call id=1
 import time
-import subprocess
 import os
-import pprint
+#import pprint
+#import subprocess
 from gevent import monkey; monkey.patch_socket()
 import gevent
 timeSleep = 15
+password = 'dm_redis_123'
 
 def redis():
         import redis
         host = '0.0.0.0'
         port = 6379
-        pool = redis.ConnectionPool(host=host, port=int(port))
+        pool = redis.ConnectionPool(host=host, port=int(port), password=password)
         return redis.Redis(connection_pool=pool)
 
-def command(file):
-        return 'dm call office-convert_call id=' + file
+def pop(key):
+        return popen('redis -a '+password+' lpop ' + key)
+
+def command(process):
+        return process
 
 def popen(command, bg=False):
         string = command
         if bg == True:
                 command = command + ' &'
-
-        print command
         process = os.popen(command)
         output = process.read()
         process.close()
-        print output
         return output
 
-# 文档转换
-def convert():
+# 定时执行进程
+def process():
         r = redis()
-        c = 'office_file'
+        c = 'dm_process'
         i = 0
         while 1:
-                file = r.lpop(c)
-                if file:
-                        g = command(file)
-                        popen(g, False)
+                value = r.lpop(c)
+                if value:
+                        g = command(value)
+                        popen(g)
                 i = i+1
                 if i >= 10:
-                        time.sleep(timeSleep)
+                        gevent.sleep(timeSleep)
                         i = 0
 
 def handle():
 	gevent.joinall([
-		gevent.spawn(git),
+		gevent.spawn(process),
 		#gevent.spawn(backup),
 	])
 
