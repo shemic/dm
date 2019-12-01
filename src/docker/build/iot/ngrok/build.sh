0860d7f978f985dfca196e4ea8614f66e390974d
dever <dm@dever.cc>
2019-12-01 11:19:19 +0000

    edit

--- src/docker/build/iot/ngrok/build.sh
+++ src/docker/build/iot/ngrok/build.sh
@@ -2,9 +2,9 @@
 
 cd $NG_HOME
 openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NG_DOMAIN" -days 5000 -out rootCA.pem
 openssl genrsa -out device.key 2048
-openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+openssl req -new -key device.key -subj "/CN=$NG_DOMAIN" -out device.csr
 openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
 cp rootCA.pem assets/client/tls/ngrokroot.crt
 cp device.crt assets/server/tls/snakeoil.crt
