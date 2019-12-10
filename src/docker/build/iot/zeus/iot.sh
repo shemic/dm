d0c1d39aa579ef70804a45c7eb5ec5023312a90d
dever <dm@dever.cc>
2019-12-10 12:32:41 +0000

    edit

--- src/docker/build/iot/zeus/iot.sh
+++ src/docker/build/iot/zeus/iot.sh
@@ -3,55 +3,57 @@ set -e
 start_iot()
 {
     cd $IOT_HOME
-    git reset --hard FETCH_HEAD
+    #git reset --hard FETCH_HEAD
     git pull
     install.py
-    process_start admin.py
-    process_start front.py
-    process_start modbus.py -m tcp_start
-    process_start modbus.py -m rtu_start
-    process_start cron.py -m control
-    process_start cron.py -m device
-    process_start cron.py -m loop
-    process_start cron.py -m queue
-    process_start cron.py -m queuedrop
-    process_start cron.py -m savepic
-    process_start cron.py -m timesync
-    process_start cron.py -m timing
-    process_start cron.py -m mqtt_sub
-    #process_start cron.py -m mqtt_pub
+    process_start python3 admin.py
+    process_start python3 front.py
+    process_start python3 web.py
+    process_start python3 modbus.py -m tcp_start
+    process_start python3 modbus.py -m rtu_start
+    process_start python3 cron.py -m control
+    process_start python3 cron.py -m device
+    process_start python3 cron.py -m loop
+    process_start python3 cron.py -m queue
+    process_start python3 cron.py -m queuedrop
+    process_start python3 cron.py -m savepic
+    process_start python3 cron.py -m timesync
+    process_start python3 cron.py -m timing
+    process_start python3 cron.py -m mqtt_sub
 }
 
 stop_iot()
 {
-	process_stop admin.py
-    process_stop front.py
-    process_stop modbus.py -m tcp_start
-    process_stop modbus.py -m rtu_start
-    process_stop cron.py -m control
-    process_stop cron.py -m device
-    process_stop cron.py -m loop
-    process_stop cron.py -m queue
-    process_stop cron.py -m queuedrop
-    process_stop cron.py -m savepic
-    process_stop cron.py -m timesync
-    process_stop cron.py -m timing
-    process_stop cron.py -m mqtt_sub
+    process_stop python3 admin.py
+    process_stop python3 front.py
+    process_stop python3 web.py
+    process_stop python3 modbus.py -m tcp_start
+    process_stop python3 modbus.py -m rtu_start
+    process_stop python3 cron.py -m control
+    process_stop python3 cron.py -m device
+    process_stop python3 cron.py -m loop
+    process_stop python3 cron.py -m queue
+    process_stop python3 cron.py -m queuedrop
+    process_stop python3 cron.py -m savepic
+    process_stop python3 cron.py -m timesync
+    process_stop python3 cron.py -m timing
+    process_stop python3 cron.py -m mqtt_sub
 }
 
 monit_iot()
 {
-    process_monit admin.py
-    process_monit front.py
-    process_monit modbus.py -m tcp_start
-    process_monit modbus.py -m rtu_start
-    process_monit cron.py -m control
-    process_monit cron.py -m device
-    process_monit cron.py -m loop
-    process_monit cron.py -m queue
-    process_monit cron.py -m queuedrop
-    process_monit cron.py -m savepic
-    process_monit cron.py -m timesync
-    process_monit cron.py -m timing
-    process_monit cron.py -m mqtt_sub
+    process_monit python3 admin.py
+    process_monit python3 front.py
+    process_monit python3 web.py
+    process_monit python3 modbus.py -m tcp_start
+    process_monit python3 modbus.py -m rtu_start
+    process_monit python3 cron.py -m control
+    process_monit python3 cron.py -m device
+    process_monit python3 cron.py -m loop
+    process_monit python3 cron.py -m queue
+    process_monit python3 cron.py -m queuedrop
+    process_monit python3 cron.py -m savepic
+    process_monit python3 cron.py -m timesync
+    process_monit python3 cron.py -m timing
+    process_monit python3 cron.py -m mqtt_sub
 }
