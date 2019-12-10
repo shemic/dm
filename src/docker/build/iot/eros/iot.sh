e2284ec42addfa96f2dbbf80765f94dc31361667
dever <dm@dever.cc>
2019-12-10 12:52:45 +0000

    edit

--- src/docker/build/iot/eros/iot.sh
+++ src/docker/build/iot/eros/iot.sh
@@ -6,54 +6,54 @@ start_iot()
     git reset --hard FETCH_HEAD
     git pull
     install.py
-    process_start {$IOT_HOME}/admin.py
-    process_start {$IOT_HOME}/front.py
-    process_start {$IOT_HOME}/modbus.py -m tcp_start
-    process_start {$IOT_HOME}/modbus.py -m rtu_start
-    process_start {$IOT_HOME}/cron.py -m control
-    process_start {$IOT_HOME}/cron.py -m device
-    process_start {$IOT_HOME}/cron.py -m loop
-    process_start {$IOT_HOME}/cron.py -m queue
-    process_start {$IOT_HOME}/cron.py -m queuedrop
-    process_start {$IOT_HOME}/cron.py -m savepic
-    process_start {$IOT_HOME}/cron.py -m timesync
-    process_start {$IOT_HOME}/cron.py -m timing
-    process_start {$IOT_HOME}/cron.py -m mqtt_sub
+    process_start python3 admin.py
+    process_start python3 front.py
+    process_start python3 modbus.py -m tcp_start
+    process_start python3 modbus.py -m rtu_start
+    process_start python3 cron.py -m control
+    process_start python3 cron.py -m device
+    process_start python3 cron.py -m loop
+    process_start python3 cron.py -m queue
+    process_start python3 cron.py -m queuedrop
+    process_start python3 cron.py -m savepic
+    process_start python3 cron.py -m timesync
+    process_start python3 cron.py -m timing
+    process_start python3 cron.py -m mqtt_sub
     #process_start cron.py -m mqtt_pub
-    process_start ngrok -subdomain="f{$IOT_FARM}" -config="{$IOT_HOME}/ngrok.cfg" 8091
+    process_start ngrok -subdomain="f$IOT_FARM" -config="ngrok.cfg" 8091
 }
 
 stop_iot()
 {
-	process_stop {$IOT_HOME}/admin.py
-    process_stop {$IOT_HOME}/front.py
-    process_stop {$IOT_HOME}/modbus.py -m tcp_start
-    process_stop {$IOT_HOME}/modbus.py -m rtu_start
-    process_stop {$IOT_HOME}/cron.py -m control
-    process_stop {$IOT_HOME}/cron.py -m device
-    process_stop {$IOT_HOME}/cron.py -m loop
-    process_stop {$IOT_HOME}/cron.py -m queue
-    process_stop {$IOT_HOME}/cron.py -m queuedrop
-    process_stop {$IOT_HOME}/cron.py -m savepic
-    process_stop {$IOT_HOME}/cron.py -m timesync
-    process_stop {$IOT_HOME}/cron.py -m timing
-    process_stop {$IOT_HOME}/cron.py -m mqtt_sub
+	process_stop python3 admin.py
+    process_stop python3 front.py
+    process_stop python3 modbus.py -m tcp_start
+    process_stop python3 modbus.py -m rtu_start
+    process_stop python3 cron.py -m control
+    process_stop python3 cron.py -m device
+    process_stop python3 cron.py -m loop
+    process_stop python3 cron.py -m queue
+    process_stop python3 cron.py -m queuedrop
+    process_stop python3 cron.py -m savepic
+    process_stop python3 cron.py -m timesync
+    process_stop python3 cron.py -m timing
+    process_stop python3 cron.py -m mqtt_sub
 }
 
 monit_iot()
 {
-    process_monit {$IOT_HOME}/admin.py
-    process_monit {$IOT_HOME}/front.py
-    process_monit {$IOT_HOME}/modbus.py -m tcp_start
-    process_monit {$IOT_HOME}/modbus.py -m rtu_start
-    process_monit {$IOT_HOME}/cron.py -m control
-    process_monit {$IOT_HOME}/cron.py -m device
-    process_monit {$IOT_HOME}/cron.py -m loop
-    process_monit {$IOT_HOME}/cron.py -m queue
-    process_monit {$IOT_HOME}/cron.py -m queuedrop
-    process_monit {$IOT_HOME}/cron.py -m savepic
-    process_monit {$IOT_HOME}/cron.py -m timesync
-    process_monit {$IOT_HOME}/cron.py -m timing
-    process_monit {$IOT_HOME}/cron.py -m mqtt_sub
-    process_monit ngrok -subdomain="f{$IOT_FARM}" -config="{$IOT_HOME}/ngrok.cfg" 8091
+    process_monit python3 admin.py
+    process_monit python3 front.py
+    process_monit python3 modbus.py -m tcp_start
+    process_monit python3 modbus.py -m rtu_start
+    process_monit python3 cron.py -m control
+    process_monit python3 cron.py -m device
+    process_monit python3 cron.py -m loop
+    process_monit python3 cron.py -m queue
+    process_monit python3 cron.py -m queuedrop
+    process_monit python3 cron.py -m savepic
+    process_monit python3 cron.py -m timesync
+    process_monit python3 cron.py -m timing
+    process_monit python3 cron.py -m mqtt_sub
+    process_monit ngrok -subdomain="f$IOT_FARM" -config="ngrok.cfg" 8091
 }
