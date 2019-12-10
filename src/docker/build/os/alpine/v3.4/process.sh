aa74e62564632c4b2af3da42fe2ebbe36d6a9108
dever <dm@dever.cc>
2019-12-10 13:26:02 +0000

    edit

--- src/docker/build/os/alpine/v3.4/process.sh
+++ src/docker/build/os/alpine/v3.4/process.sh
@@ -2,7 +2,7 @@
 set -e
 process_status()
 {
-    pids=`ps aux|grep '$*'|grep -v entrypoint|grep -v grep|awk '{print $*}'`
+    pids=`ps aux|grep '$*'|grep -v entrypoint|grep -v grep|awk '{print $1}'`
     if [ "$pids" ]; then
         echo $pids
     else
